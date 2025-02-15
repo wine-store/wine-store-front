a60fa82e96ead69fceca518e636e32334eef8a1b
Ann Busyhina <070196ann@gmail.com>
2025-02-15 19:59:38 +0200

    added ProductCart

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,5 +1,35 @@
-export const ProductCard = () => {
+import { Link } from 'react-router-dom';
+import styles from './ProductCard.module.scss';
+import { Wine } from '../../types/Wine';
+import { FaStar } from 'react-icons/fa'; 
+type Props = {
+  category: 'all' | 'wine' | 'object' | 'certificate';
+  wine: Wine;
+};
+export const ProductCard: React.FC<Props> = ({ category, wine }) => {
   return (
-    <div>Product 1</div>
-  )
-}
+    <article className={styles.wineCard}>
+      <Link to={category === 'all' ? `/${wine.itemId}` : `/${category}/${wine.itemId}`} className={styles.picture}>
+        <div className={styles.imageContainer}>
+        <img
+          src={'https://placehold.jp/330x360.png'}
+          alt={wine.title}
+          className={styles.image}
+          />
+           <div className={styles.rating}>
+            {/* add rating */}
+            <span className={styles.ratingValue}>5</span> 
+            <FaStar className={styles.starIcon} />
+          </div>
+          </div>
+      </Link>
+      <div className={styles.content}>
+      <Link to={`/${category}/${wine.itemId}`} className={styles.title}>
+        {wine.title}
+      </Link>
+      <div className={styles.price}>{`${wine.price} $`}</div>
+      </div>
+      <div className={styles.item}>{wine.vintage}</div>
+    </article>
+  );
+};
--- src/module/CatalogPage/CatalogPage.tsx
+++ src/module/CatalogPage/CatalogPage.tsx
@@ -7,6 +7,180 @@ type CatalogPageProps = {
   category: 'all' | 'wine' | 'object' | 'certificate';
 };
 
+const wine = [
+  {
+    itemId: "1",
+    title: "Settesoli Pinot Grigio",
+    description:
+      "A refined white wine from England, specifically the Surrey region. Offers flavors of pineapple, leather, coffee and is best described as Crisp & Zesty.",
+    price: 40.0,
+    capacity: 0.75,
+    grape: "Orvieto",
+    closure: "Natural Cork",
+    country: "England",
+    characteristics: ["pineapple", "leather", "coffee"],
+    type: "White",
+    ABV: 14.0,
+    region: "Surrey",
+    style: "Crisp & Zesty",
+    vintage: 1998,
+  },
+  {
+    itemId: "2",
+    title: "Settesoli Cabernet Sauvignon",
+    description:
+      "A refined red wine from Chile, specifically the Chile region. Offers flavors of black fruit, blackberry, black fruit and is best described as Delicate & Dry.",
+    price: 43.0,
+    capacity: 0.75,
+    grape: "Cabernet Sauvignon",
+    closure: "Natural Cork",
+    country: "Chile",
+    characteristics: ["black fruit", "blackberry", "black fruit"],
+    type: "Red",
+    ABV: 14.0,
+    region: "Chile",
+    style: "Delicate & Dry",
+    vintage: 2011,
+  },
+  {
+    itemId: "3",
+    title: "Jam Shed Chardonnay",
+    description:
+      "A refined white wine from Hungary. Offers flavors of green pepper, clove, cinnamon and is best described as Soft & Fruity.",
+    price: 31.0,
+    capacity: 0.75,
+    grape: "Chardonnay",
+    closure: "Natural Cork",
+    country: "Hungary",
+    characteristics: ["green pepper", "clove", "cinnamon"],
+    type: "White",
+    ABV: 13.5,
+    region: "Hungary",
+    style: "Soft & Fruity",
+    vintage: 2009,
+  },
+  {
+    itemId: "4",
+    title: "Jam Shed Shiraz",
+    description:
+      "A refined red wine from New Zealand, specifically the Marlborough region. Offers flavors of mango, blueberry, strawberry and is best described as Rich & Juicy.",
+    price: 55.0,
+    capacity: 0.75,
+    grape: "Primitivo",
+    closure: "Natural Cork",
+    country: "New Zealand",
+    characteristics: ["mango", "blueberry", "strawberry"],
+    type: "Red",
+    ABV: 13.0,
+    region: "Marlborough",
+    style: "Rich & Juicy",
+    vintage: 2019,
+  },
+  {
+    itemId: "5",
+    title: "Hardys Stamp Shiraz",
+    description:
+      "A refined red wine from Romania. Offers flavors of green apple, caramel, black fruit and is best described as Soft & Fruity.",
+    price: 6.0,
+    capacity: 0.75,
+    grape: "Amarone",
+    closure: "Natural Cork",
+    country: "Romania",
+    characteristics: ["green apple", "caramel", "black fruit"],
+    type: "Red",
+    ABV: 13.0,
+    region: "Romania",
+    style: "Soft & Fruity",
+    vintage: 2024,
+  },
+  {
+    itemId: "6",
+    title: "Hardys Stamp Cabernet Sauvignon",
+    description:
+      "A refined red wine from Lebanon. Offers flavors of earth, blueberry, berries and is best described as Soft & Fruity.",
+    price: 12.55,
+    capacity: 0.75,
+    grape: "Cabernet Sauvignon",
+    closure: "Natural Cork",
+    country: "Lebanon",
+    characteristics: ["earth", "blueberry", "berries"],
+    type: "Red",
+    ABV: 12.0,
+    region: "Lebanon",
+    style: "Soft & Fruity",
+    vintage: 2004,
+  },
+  {
+    itemId: "7",
+    title: "Hardys Stamp Chardonnay",
+    description:
+      "A refined white wine from Georgia. Offers flavors of blackberry, minerals, leather and is best described as Crisp & Zesty.",
+    price: 33.0,
+    capacity: 0.75,
+    grape: "Chardonnay",
+    closure: "Natural Cork",
+    country: "Georgia",
+    characteristics: ["blackberry", "minerals", "leather"],
+    type: "White",
+    ABV: 12.5,
+    region: "Kvareli",
+    style: "Crisp & Zesty",
+    vintage: 2023,
+  },
+  {
+    itemId: "8",
+    title: "Hardys Stamp Rose",
+    description:
+      "A refined rose wine from Lebanon. Offers flavors of honey, fruit, leather and is best described as Crisp & Fruity.",
+    price: 34.0,
+    capacity: 0.75,
+    grape: "Pinot Noir",
+    closure: "Natural Cork",
+    country: "Lebanon",
+    characteristics: ["honey", "fruit", "leather"],
+    type: "Rose",
+    ABV: 13.5,
+    region: "Lebanon",
+    style: "Crisp & Fruity",
+    vintage: 1998,
+  },
+  {
+    itemId: "9",
+    title: "Banrock Station Shiraz",
+    description:
+      "A refined red wine from Argentina, specifically the Mendoza region. Offers flavors of fruit, mushrooms, nuts and is best described as Savoury & Full Bodied.",
+    price: 53.44,
+    capacity: 0.75,
+    grape: "Marsanne",
+    closure: "Natural Cork",
+    country: "Argentina",
+    characteristics: ["fruit", "mushrooms", "nuts"],
+    type: "Red",
+    ABV: 11.5,
+    region: "Mendoza",
+    style: "Savoury & Full Bodied",
+    vintage: 2016,
+  },
+  {
+    itemId: "10",
+    title: "Banrock Station Chardonnay",
+    description:
+      "A refined white wine from England, specifically the Hampshire region. Offers flavors of sweetness, spice, tropical fruit and is best described as Crisp & Zesty.",
+    price: 7.0,
+    capacity: 0.75,
+    grape: "Chardonnay",
+    closure: "Natural Cork",
+    country: "England",
+    characteristics: ["sweetness", "spice", "tropical fruit"],
+    type: "White",
+    ABV: 12.5,
+    region: "Hampshire",
+    style: "Crisp & Zesty",
+    vintage: 2009,
+  },
+]
+
+
 export const CatalogPage: React.FC<CatalogPageProps> = ({ category }) => {
   const [isFilterOpen, setIsFilterOpen] = useState(false);
   const toggleFilterVisibility = () => {
@@ -19,9 +193,11 @@ export const CatalogPage: React.FC<CatalogPageProps> = ({ category }) => {
       <section className={styles.catalog__content}>
         <FilterForWine category={category} toggleFilterVisibility={toggleFilterVisibility} isFilterOpen={isFilterOpen} />
         <ul className={styles.cardsConteiner}>
-          <li className={styles.card}>
-            <ProductCard />
-          </li>
+          {wine.map(wine => (
+            <li className={styles.card}>
+              <ProductCard category={category} wine={wine} />
+            </li>
+          ))}
         </ul>
       </section>
     </article>
